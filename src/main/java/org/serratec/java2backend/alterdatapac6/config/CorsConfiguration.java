4a0eb96392b2f72a7304e1ca58c9f2d4a53081a7
andradeClayton <sussatere@gmail.com>
2021-07-27 15:51:46 -0300

    inclusão fo headerExposed, tirando o fdp do ;

--- src/main/java/org/serratec/java2backend/alterdatapac6/config/CorsConfiguration.java
+++ src/main/java/org/serratec/java2backend/alterdatapac6/config/CorsConfiguration.java
@@ -7,12 +7,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+	
+	
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+            .exposedHeaders("Access-Control-Allow-Headers", "Authorization, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
+                    "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            
+        
+            
+        
     }
+    
+    
 }
 
 
