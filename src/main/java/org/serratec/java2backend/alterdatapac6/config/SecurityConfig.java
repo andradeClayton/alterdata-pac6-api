0994a2de0bdb9a17a006a8b9d4ce96a508a05ea7
andradeClayton <sussatere@gmail.com>
2021-07-29 10:38:53 -0300

    restesenha

--- src/main/java/org/serratec/java2backend/alterdatapac6/config/SecurityConfig.java
+++ src/main/java/org/serratec/java2backend/alterdatapac6/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	JWTUtil jwtUtil;
 	
-	private static final String[] AUTH_WHITELIST = {"*/resetSenha/*"};
+	private static final String[] AUTH_WHITELIST = {"**/resetSenha/**"};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
